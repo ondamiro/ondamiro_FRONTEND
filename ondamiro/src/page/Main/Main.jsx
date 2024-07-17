4910900ec1992b75ba31ec38425086540ea806e5
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-07-17 18:02:33 +0900

    메인 화면 일단 저장

--- ondamiro/src/page/Main/Main.jsx
+++ ondamiro/src/page/Main/Main.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import foodImg1 from '../../assets/img/image 7foodImgs.svg'
-import foodImg2 from '../../assets/img/image 8foodImgs.svg'
-import foodImg3 from '../../assets/img/image 9foodImgs.svg'
-import foodImg4 from '../../assets/img/image 10foodImgs.svg'
-import foodImg5 from '../../assets/img/image 11foodImgs.svg'
-import foodImg6 from '../../assets/img/image 12foodImgs.svg'
-import foodImg7 from '../../assets/img/image 13foodImgs.svg'
-import foodImg8 from '../../assets/img/image 14foodImgs.svg'
-
-
+import React from "react"
+import foodImg1 from "../../assets/img/image 7foodImgs.svg"
+import foodImg2 from "../../assets/img/image 8foodImgs.svg"
+import foodImg3 from "../../assets/img/image 9foodImgs.svg"
+import foodImg4 from "../../assets/img/image 10foodImgs.svg"
+import foodImg5 from "../../assets/img/image 11foodImgs.svg"
+import foodImg6 from "../../assets/img/image 12foodImgs.svg"
+import foodImg7 from "../../assets/img/image 13foodImgs.svg"
+import foodImg8 from "../../assets/img/image 14foodImgs.svg"
 
 const Main = () => {
   return (
     <>
-    <div>
+      <div>
         <div>음식 나눔</div>
         <div>
           <div>
-            <img src={foodImg1} alt='error' />
+            <img src={foodImg1} alt="error" />
             <p>3분 전에 완성된 3분 카레라이스 드실분~</p>
             <div>
               <p>1인분</p>
@@ -25,9 +23,9 @@ const Main = () => {
             </div>
           </div>
         </div>
-    </div>
+      </div>
     </>
   )
 }
 
-export default Main;
+export default Main
