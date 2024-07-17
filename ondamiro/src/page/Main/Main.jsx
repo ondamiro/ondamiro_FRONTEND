8c60616aaa311beb424a0f7364998512d19e76f3
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-07-17 20:06:11 +0900

    mainpage 일단 저장

--- ondamiro/src/page/Main/Main.jsx
+++ ondamiro/src/page/Main/Main.jsx
@@ -1,14 +1,42 @@
-import React from "react"
-import foodImg1 from "../../assets/img/image 7foodImgs.svg"
-import foodImg2 from "../../assets/img/image 8foodImgs.svg"
-import foodImg3 from "../../assets/img/image 9foodImgs.svg"
-import foodImg4 from "../../assets/img/image 10foodImgs.svg"
-import foodImg5 from "../../assets/img/image 11foodImgs.svg"
-import foodImg6 from "../../assets/img/image 12foodImgs.svg"
-import foodImg7 from "../../assets/img/image 13foodImgs.svg"
-import foodImg8 from "../../assets/img/image 14foodImgs.svg"
+import React, { useEffect, useState } from "react"
+import * as S from "./Main.styled";
 
 const Main = () => {
+  const { id } = useParams();
+  const Navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [foodImg, setFoodImg] = useState("");
+  const [howMuch, setHowMuch] = useState("");
+  const [ingrdint, setIngrdint] = useState("");
+  const [exprationDate, setExprationDate] = useState("");
+  const [time, setTime] = useState("");
+  const [map, setMap] = useState("");
+  const [user, setUser] = useState("");
+  const [recipe, setRecipe] = useState("");
+  const [special, setSpecial] = useState("");
+
+  useEffect(() => {
+    if (id) {
+      // JSON 데이터에서 id에 해당하는 데이터를 찾습니다.
+      const postData = mainData.find(data => data.id === parseInt(id));
+
+      if (postData) {
+        setTitle(postData.title);
+        setFoodImg(postData.foodImg);
+        setHowMuch(postData.howMuch);
+        setIngredient(postData.ingredient);
+        setExpirationDate(postData.expirationDate);
+        setTime(postData.time);
+        setMap(postData.map);
+        setUser(postData.user);
+        setRecipe(postData.recipe);
+        setSpecial(postData.special);
+      } else {
+        console.error("Post not found");
+      }
+    }
+  }, [id]);
+
   return (
     <>
       <div>
