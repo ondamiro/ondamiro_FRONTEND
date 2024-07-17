9ce3316ca5965e229ae776c6ff784ac262fc2791
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-07-18 01:18:29 +0900

    dummy edit

--- ondamiro/src/page/Main/Main.jsx
+++ ondamiro/src/page/Main/Main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import data  from "../Main/data.json"
+import Topbar from "../../common/Topbar/Topbar"
 
 const Main = () => {
   console.log(data.items)
   return (
     <>
+    <Topbar />
     {data.items.map(data=>(<>
     <div key={data.id}>
       <div>{data.title}</div>
-      <div>{data.content}</div>
+      <div>{data.time}</div>
+      <div>{data.person}</div>
     </div>
     </>))}
     </>
