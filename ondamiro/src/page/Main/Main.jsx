d24d597bebe84a5e5f9f18e285c85e49ff120334
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-07-17 20:39:16 +0900

    mainPage

--- ondamiro/src/page/Main/Main.jsx
+++ ondamiro/src/page/Main/Main.jsx
@@ -1,43 +1,43 @@
 import React, { useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router-dom";
-import mainData from "./MainData.json";
-import * as S from "./Main.styled";
+import { useNavigate, useParams } from "react-router-dom"
+import mainData from "./MainData.json"
+import * as S from "./Main.styled"
 
 const Main = () => {
-  const { id } = useParams();
-  const Navigate = useNavigate();
-  const [title, setTitle] = useState("");
-  const [foodImg, setFoodImg] = useState("");
-  const [howMuch, setHowMuch] = useState("");
-  const [ingrdint, setIngrdint] = useState("");
-  const [exprationDate, setExprationDate] = useState("");
-  const [time, setTime] = useState("");
-  const [map, setMap] = useState("");
-  const [user, setUser] = useState("");
-  const [recipe, setRecipe] = useState("");
-  const [special, setSpecial] = useState("");
+  const { id } = useParams()
+  const Navigate = useNavigate()
+  const [title, setTitle] = useState("")
+  const [foodImg, setFoodImg] = useState("")
+  const [howMuch, setHowMuch] = useState("")
+  const [ingrdint, setIngrdint] = useState("")
+  const [exprationDate, setExprationDate] = useState("")
+  const [time, setTime] = useState("")
+  const [map, setMap] = useState("")
+  const [user, setUser] = useState("")
+  const [recipe, setRecipe] = useState("")
+  const [special, setSpecial] = useState("")
 
   useEffect(() => {
     if (id) {
       // JSON 데이터에서 id에 해당하는 데이터를 찾습니다.
-      const postData = mainData.find(data => data.id === parseInt(id));
+      const postData = mainData.find((data) => data.id === parseInt(id))
 
       if (postData) {
-        setTitle(postData.title);
-        setFoodImg(postData.foodImg);
-        setHowMuch(postData.howMuch);
-        setIngredient(postData.ingredient);
-        setExpirationDate(postData.expirationDate);
-        setTime(postData.time);
-        setMap(postData.map);
-        setUser(postData.user);
-        setRecipe(postData.recipe);
-        setSpecial(postData.special);
+        setTitle(postData.title)
+        setFoodImg(postData.foodImg)
+        setHowMuch(postData.howMuch)
+        setIngrdint(postData.ingredient)
+        setExprationDate(postData.expirationDate)
+        setTime(postData.time)
+        setMap(postData.map)
+        setUser(postData.user)
+        setRecipe(postData.recipe)
+        setSpecial(postData.special)
       } else {
-        console.error("Post not found");
+        console.error("Post not found")
       }
     }
-  }, [id]);
+  }, [id])
 
   return (
     <>
@@ -45,11 +45,11 @@ const Main = () => {
         <div>음식 나눔</div>
         <div>
           <div>
-            <img src={foodImg1} alt="error" />
-            <p>3분 전에 완성된 3분 카레라이스 드실분~</p>
+            <img src={foodImg} alt="error" />
+            <p>{title}</p>
             <div>
-              <p>1인분</p>
-              <p>1분 전</p>
+              <p>{howMuch}</p>
+              <p>{time}</p>
             </div>
           </div>
         </div>
