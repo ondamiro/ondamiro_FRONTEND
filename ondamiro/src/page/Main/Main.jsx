28f3bc84766ed23cc310d8b195118233ee3f8718
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-07-17 16:35:13 +0900

    mainPage

--- ondamiro/src/page/Main/Main.jsx
+++ ondamiro/src/page/Main/Main.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import foodImg1 from '../../assets/img/image 7foodImgs.svg'
+import foodImg2 from '../../assets/img/image 8foodImgs.svg'
+import foodImg3 from '../../assets/img/image 9foodImgs.svg'
+import foodImg4 from '../../assets/img/image 10foodImgs.svg'
+import foodImg5 from '../../assets/img/image 11foodImgs.svg'
+import foodImg6 from '../../assets/img/image 12foodImgs.svg'
+import foodImg7 from '../../assets/img/image 13foodImgs.svg'
+import foodImg8 from '../../assets/img/image 14foodImgs.svg'
+
+
 
 const Main = () => {
   return (
+    <>
     <div>
+        <div>음식 나눔</div>
         <div>
-            
+          <div>
+            <img src={foodImg1} alt='error' />
+            <p>3분 전에 완성된 3분 카레라이스 드실분~</p>
+            <div>
+              <p>1인분</p>
+              <p>1분 전</p>
+            </div>
+          </div>
         </div>
     </div>
+    </>
   )
 }
 
---
+++ ondamiro/src/page/Main/Main.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const back = styled.div`
+background-color: red;
+  width: 30vw;
+`;
