08b6f6d8b904c93b9171212ee0f5fe2cb2289df1
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-07-17 21:03:05 +0900

    라우터 설정 완료

--- ondamiro/src/App.js
+++ ondamiro/src/App.js
@@ -1,13 +1,16 @@
 import Main from "./page/Main/Main"
 import "./App.css"
 // import { BrowseRouter } from "react-router-dom"
-import BrowseRouter from "re"
+// import { BrowseRouter } from "react-router-dom"
+import Router from "./component/router"
+// import BrowseRouter from "re"
 function App() {
   return (
     // <div>
-    <BrowseRouter>
-      <Main />
-    </BrowseRouter>
+    // <BrowseRouter>
+    <Router />
+    // <Main />
+    // </BrowseRouter>
     // </div>
   )
 }
--- ondamiro/src/component/router.jsx
+++ ondamiro/src/component/router.jsx
@@ -1,18 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GlobalStyle from "src/style/global.style";
-import Main from "../page/Main/Main";
-import Topbar from "../content/Topbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import GlobalStyle from "src/style/global.style"
+import GlobalStyle from "../style/global.style"
+import Main from "../page/Main/Main"
+import Topbar from "../content/Topbar"
+import Login from "../component/Auth/Login/Login"
+import SignUp from "../component/Auth/SignUp/SignUp"
 
 const Router = () => {
-    return (
-      <BrowserRouter>
-        <GlobalStyle />
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="topbar" element={<Topbar />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  };
-  
-  export default Router;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="topbar" element={<Topbar />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
