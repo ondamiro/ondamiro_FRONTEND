dcb6a8e254f2a8cb21331d012c89d2b79cbee078
kwondongwoo0424 <144919720+kwondongwoo0424@users.noreply.github.com>
2024-07-17 20:09:12 +0900

    로그인페이지 완성

--- ondamiro/src/App.js
+++ ondamiro/src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Login from "./component/Auth/Login/Login";
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Login />
     </div>
   );
 }
