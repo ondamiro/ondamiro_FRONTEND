ff833c032445eca7447da93241db15a4fe353cd4
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2024-07-18 06:47:14 +0900

    shopping page done

---
+++ ondamiro/src/common/ShopBox/ShopBox.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import data from "../../page/ShopPage/shopData.json";
+import * as S from "./ShopBox.styled";
+import { useNavigate } from "react-router-dom";
+
+const ShopBox = () => {
+  const Navigate = useNavigate();
+
+  return (
+    <>
+      {data.items.map((data) => (
+        <S.Back onClick={() => Navigate(`/ShopDetail/${data.id}`)}>
+          <img src={data.img} alt="error" />
+          <S.Area>
+            <S.textArea key={data.id}>
+              <S.Title>{data.title}</S.Title>
+              <S.bottomArea>
+                <S.Person>{data.company}</S.Person>
+                <S.Time>{data.person}</S.Time>
+              </S.bottomArea>
+            </S.textArea>
+          </S.Area>
+        </S.Back>
+      ))}
+    </>
+  );
+};
+
+export default ShopBox;
---
+++ ondamiro/src/common/ShopBox/ShopBox.styled.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Back = styled.div`
+width: 33vw;
+height: 15vh;
+border-radius: 10px;
+background-color: #FBF9F9;
+display: flex;
+align-items: center;
+justify-content: space-around;
+`;
+
+export const Area = styled.div`
+width: 20vw;
+display: flex;
+justify-content: space-between;
+`;
+
+export const bottomArea = styled.div`
+width: 20vw;
+display: flex;
+justify-content: space-between;
+`;
+
+export const textArea = styled.div`
+width: 20vw;
+height: 7.5vh;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+`;
+
+export const Title = styled.div`
+font-size: 18px;
+font-weight: 600;
+`;
+
+export const Person = styled.div`
+font-size: 15px;
+font-weight: 430;
+`;
+
+export const Time = styled.div`
+font-size: 13px;
+font-weight: 500;
+color: #A8A8A8;
+`;
